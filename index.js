b662e8da6e90aef8d58d3e77f5a4792d65cd533d
Pamela Frieze <pfrieze@gmail.com>
2019-02-10 16:45:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function driverNamesWithRevenueOver (drivers, revenue) {
     });
 }
 
-function exactMatch(drivers, key: attribute) {
+function exactMatch(drivers, key) {
   return drivers.filter(function(driver) {
     return driver[key] === attribute[key]; });
 }
