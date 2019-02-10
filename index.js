397dc266d0916bced6b49e5925797f999ae6dde2
Pamela Frieze <pfrieze@gmail.com>
2019-02-10 17:39:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function driverNamesWithRevenueOver (drivers, revenue) {
 function exactMatch(drivers, obj) {
   return drivers.filter(function(driver) {
     Prop = Object.getOwnPropertyNames(obj)
-    return Prop === obj; });
+    return Prop === driver; });
 }
